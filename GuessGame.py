265153bfb8079f966c23d7e21636c7adc7a1c615
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2020-03-14 12:10:13 +0100

    Move to the left the code.

--- GuessGame.py
+++ GuessGame.py
@@ -10,65 +10,65 @@ guessesTaken = 0
 answer = 0
 
 if answer == 0:
-    for guessesTaken in range (5):
-        print("Try to guess it.")
-        guessNumber = input("")
-        guessNumber = int(guessNumber)
+  for guessesTaken in range (5):
+    print("Try to guess it.")
+    guessNumber = input("")
+    guessNumber = int(guessNumber)
 
-        if guessNumber > number :
-            print("Nope ! Too high. Try again.")
-            guessesTaken += 1
+    if guessNumber > number :
+        print("Nope ! Too high. Try again.")
+        guessesTaken += 1
 
-        if guessNumber < number :
-            print("It's too low. Try again !")
-            guessesTaken += 1
+    if guessNumber < number :
+        print("It's too low. Try again !")
+        guessesTaken += 1
 
-        if guessNumber == number :
-            guessesTaken += 1
-            answer = 1
-            break
-
-if answer == 0:
-        print("Try to guess it.")
-        guessNumber = input("")
-        guessNumber = int(guessNumber)
-
-        if guessNumber > number :
-            print("Nope ! Too high. Last try.")
-            guessesTaken += 1
-
-        if guessNumber < number :
-            print("It's too low. Last try kid.")
-            guessesTaken += 1
-
-        if guessNumber == number :
-            guessesTaken += 1
-            answer = 1
-
+    if guessNumber == number :
+        guessesTaken += 1
+        answer = 1
+        break
 
 if answer == 0:
-        print("Try to guess it.")
-        guessNumber = input("")
-        guessNumber = int(guessNumber)
+  print("Try to guess it.")
+  guessNumber = input("")
+  guessNumber = int(guessNumber)
 
-        if guessNumber > number :
-            print("Nope ! Too high.")
-            guessesTaken += 1
+  if guessNumber > number :
+      print("Nope ! Too high. Last try.")
+      guessesTaken += 1
 
-        if guessNumber < number :
-            print("It's too low.")
-            guessesTaken += 1
+  if guessNumber < number :
+      print("It's too low. Last try kid.")
+      guessesTaken += 1
 
-        if guessNumber == number :
-            guessesTaken += 1
-            answer = 1
+  if guessNumber == number :
+      guessesTaken += 1
+      answer = 1
+
+
+if answer == 0:
+  print("Try to guess it.")
+  guessNumber = input("")
+  guessNumber = int(guessNumber)
+
+  if guessNumber > number :
+      print("Nope ! Too high.")
+      guessesTaken += 1
+
+  if guessNumber < number :
+      print("It's too low.")
+      guessesTaken += 1
+
+  if guessNumber == number :
+      guessesTaken += 1
+      answer = 1
 
 if guessNumber == number :
-    guessesTaken = str(guessesTaken)
-    number = str(number)
-    print(f"Hey ! You find the right number in {guessesTaken} attempts. The number was indeed the number {number}.")
-    number = int(number)
+  guessesTaken = str(guessesTaken)
+  number = str(number)
+  print(f"Hey ! You find the right number in {guessesTaken} attempts. The number was indeed the number {number}.")
+  number = int(number)
 
 if guessNumber != number :
-    number = str(number)
-    print(f"Too bad for you, the number was {number}.")
+  number = str(number)
+  print(f"Too bad for you, the number was {number}.")
